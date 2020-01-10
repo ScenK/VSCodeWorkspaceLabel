1102ca76423b922bf26bedee09f84023504b5e8c
Scen <scen.dete@gmail.com>
2020-01-10 16:13:49 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Features
+
+It's just a small label on the bottom right showing the current workspace folder.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+-----------------------------------------------------------------------------------------------------------
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,36 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode'
+import { StatusBarAlignment, StatusBarItem, window, workspace } from 'vscode'
+
+export function activate(context: vscode.ExtensionContext) {
+	const workspaceLabel = new WorkspaceLabel()
+
+	context.subscriptions.push(workspaceLabel)
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
+
+export class WorkspaceLabel {
+	private statusBarItem: StatusBarItem
+	private workspaceFolderName: vscode.WorkspaceFolder[] | undefined
+
+	constructor() {
+		this.statusBarItem = window.createStatusBarItem(StatusBarAlignment.Right)
+		this.workspaceFolderName = workspace.workspaceFolders
+		this.updateWorkSpaceLabel()
+	}
+
+	updateWorkSpaceLabel() {
+		if (this.workspaceFolderName) {
+			this.statusBarItem.color = '#D73900'
+			this.statusBarItem.text = `[ ${this.workspaceFolderName[0].name} ]`
+			this.statusBarItem.show()
+		}
+	}
+
+	dispose() {
+		this.statusBarItem.dispose()
+	}
+}
